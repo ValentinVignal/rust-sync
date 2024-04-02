f11da857bd29d8df012be866eff8bce4730fe377
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-04-02 14:43:31 +0800

    :sparkles: Hello world with actix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use sqlx::postgres::PgPoolOptions;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://root:password@localhost:5555/test")
-        .await?;
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
 
-    assert_eq!(row.0, 150);
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
 
-    Ok(())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }

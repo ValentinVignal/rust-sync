7499c01e5f4040339fca94e341900b8b2e408d32
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-04-16 10:38:25 +0800

    :loud_sound: Log request time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, time::Instant};
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
@@ -6,8 +6,11 @@ mod requests;
 
 #[get("/echo")]
 async fn echo(req_body: String) -> impl Responder {
+    let start = Instant::now();
     log::debug!("Echo {}", req_body);
-    HttpResponse::Ok().body(req_body)
+    let response = HttpResponse::Ok().body(req_body);
+    log::debug!("Echo took {:?}", start.elapsed());
+    response
 }
 
 #[post("/seed")]
@@ -32,9 +35,12 @@ async fn drop() -> impl Responder {
 
 #[get("/sync")]
 async fn sync() -> Result<impl Responder, Box<dyn Error>> {
+    let start = Instant::now();
     log::debug!("Sync request");
     let result = requests::sync::sync().await?;
-    Ok(web::Json(result))
+    let response = Ok(web::Json(result));
+    log::debug!("Sync took {:?}", start.elapsed());
+    response
 }
 
 #[actix_web::main]
